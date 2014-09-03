bd2f3bf399bfaaa658a037876523c6e259f24c4a
Ferdinand De-Lumban De-Ramos <ferdinand.delumban.deramos@gmail.com>
2014-09-03 15:26:36 +0000

    Sample program for testing censorify_fdr module.

---
+++ readwords/readwords.js
@@ -0,0 +1,6 @@
+var censor = require("censorify_fdr");
+console.log(censor.getCensoredWords());
+console.log(censor.censor("Some very sad, bad and mad text."));
+censor.addCensoredWord("gloomy");
+console.log(censor.getCensoredWords());
+console.log(censor.censor("A very gloomy day. Some very sad, bad and mad text."));
