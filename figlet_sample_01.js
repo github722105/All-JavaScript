35b3e3e0bc3cfb981df2bf27e14951915721ff4c
Ferdinand De-Lumban De-Ramos <ferdinand.delumban.deramos@gmail.com>
2014-09-03 15:30:44 +0000

    Changed the text.

--- figlet_sample_01.js
+++ figlet_sample_01.js
@@ -1,7 +1,7 @@
 // Sample JS program using figlet module.
 
 var figlet = require("figlet");
-figlet.text("Lumban Academy\n\n  \n\nClass of 1974", function(error, data) {
+figlet.text("FERDINAND DERAMOS", function(error, data) {
     if (error)
         console.error(error);
     else
