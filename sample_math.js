df19f826f38fde5026c40d8e70eb57fb8cf401a4
Ferdinand De-Lumban De-Ramos <ferdinand.delumban.deramos@gmail.com>
2014-09-04 13:45:14 +0000

    Sample math.

---
+++ sample_math.js
@@ -0,0 +1,8 @@
+// Sample math program.
+
+function randomPointOnCircle(radius) {
+    var angle = Math.random() * 2 * Math.PI;
+    return {x: radius * Math.cos(angle), y: radius * Math.sin(angle)};
+}
+
+console.log(randomPointOnCircle(5));
