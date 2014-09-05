ed9b0fa29e5638d29bded627ed16edbd986189e6
Ferdinand De-Lumban De-Ramos <ferdinand.delumban.deramos@gmail.com>
2014-09-05 05:37:54 +0000

    More testing practice...

--- some_testing.js
+++ some_testing.js
@@ -45,4 +45,42 @@ function lastElement(array) {
 
 
 console.log(lastElement([1, 2, 3, 4, 5,]));
-console.log(lastElement([]));             
+console.log(lastElement([""]));             
+
+var box = {
+  locked: true,
+  unlock: function() { this.locked = false; },
+  lock: function() { this.locked = true; },
+  _content: [],
+  get content() {
+      if (this.locked) throw new Error("Locked!");
+      return this._content;
+  }
+};
+
+function withBoxUnlocked(body) {
+  var locked = box.locked;
+  if (!locked)
+    return body();
+
+  box.unlock();
+  try {
+    return body();
+  } finally {
+    box.lock();
+  }
+}
+
+withBoxUnlocked(function() {
+  box.content.push("gold piece");
+});
+
+try {
+  withBoxUnlocked(function() {
+    throw new Error("Pirates on the horizon! Abort!");
+  });
+} catch (e) {
+  console.log("Error raised:", e);
+}
+
+console.log(box.locked);
